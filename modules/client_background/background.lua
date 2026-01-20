2656109d9e7d294ad2634610147efc7602a97d3e
tiagoebone <tiagoebone@gmail.com>
2026-01-19 21:01:56 -0300

    esconde versão da tela inicial

--- modules/client_background/background.lua
+++ modules/client_background/background.lua
@@ -9,11 +9,13 @@ function init()
 
   clientVersionLabel = background:getChildById('clientVersionLabel')
   clientVersionLabel:setText('OTClientV8 ' ..
-  g_app.getVersion() .. '\nrev ' .. g_app.getBuildRevision() .. '\nMade by:\n' .. g_app.getAuthor() .. "")
+    g_app.getVersion() .. '\nrev ' .. g_app.getBuildRevision() .. '\nMade by:\n' .. g_app.getAuthor() .. "")
+  clientVersionLabel:hide() -- Esconde o label de versão
 
-  if not g_game.isOnline() then
-    addEvent(function() g_effects.fadeIn(clientVersionLabel, 1500) end)
-  end
+
+  -- if not g_game.isOnline() then
+  --   addEvent(function() g_effects.fadeIn(clientVersionLabel, 1500) end)
+  -- end
 
   connect(g_game, { onGameStart = hide })
   connect(g_game, { onGameEnd = show })
