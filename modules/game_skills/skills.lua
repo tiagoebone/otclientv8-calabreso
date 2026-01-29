e01affba888df62c12e55590e7694b7566ad3f5a
tiagoebone <tiagoebone@gmail.com>
2026-01-29 18:17:28 -0300

    opcode stats

--- modules/game_skills/skills.lua
+++ modules/game_skills/skills.lua
@@ -1,5 +1,22 @@
 skillsWindow = nil
 skillsButton = nil
+manaLeechValue = 0
+lifeLeechValue = 0
+magicLevelPointsValue = 0
+skillSwordValue = 0
+skillAxeValue = 0
+skillClubValue = 0
+skillDistanceValue = 0
+skillShieldValue = 0
+extraReflectValue = 0
+extraSpeedValue = 0
+extraManaPercentValue = 0
+extraHealthPercentValue = 0
+absorbPercentPhysicalValue = 0
+absorbPercentDeathValue = 0
+absorbPercentFireValue = 0
+absorbPercentEnergyValue = 0
+absorbPercentEarthValue = 0
 
 function init()
   connect(LocalPlayer, {
@@ -30,6 +47,9 @@ function init()
   skillsButton:setOn(true)
   skillsWindow = g_ui.loadUI('skills', modules.game_interface.getRightPanel())
 
+  -- Registra callback para opcode 233 (player stats)
+  ProtocolGame.registerExtendedOpcode(233, onPlayerStatsOpcode)
+
   refresh()
   skillsWindow:setup()
 end
@@ -57,6 +77,9 @@ function terminate()
     onGameStart = refresh,
     onGameEnd = offline
   })
+
+  -- Desregistra callback do opcode 233
+  ProtocolGame.unregisterExtendedOpcode(233)
 
   skillsWindow:destroy()
   skillsButton:destroy()
@@ -212,6 +235,23 @@ function refresh()
   onOfflineTrainingChange(player, player:getOfflineTrainingTime())
   onRegenerationChange(player, player:getRegenerationTime())
   onSpeedChange(player, player:getSpeed())
+  onManaLeechChange()
+  onLifeLeechChange()
+  onMagicLevelPointsChange()
+  onSkillSwordChange()
+  onSkillAxeChange()
+  onSkillClubChange()
+  onSkillDistanceChange()
+  onSkillShieldChange()
+  onExtraReflectChange()
+  onExtraSpeedChange()
+  onExtraManaPercentChange()
+  onExtraHealthPercentChange()
+  onAbsorbPercentPhysicalChange()
+  onAbsorbPercentDeathChange()
+  onAbsorbPercentFireChange()
+  onAbsorbPercentEnergyChange()
+  onAbsorbPercentEarthChange()
 
   local hasAdditionalSkills = g_game.getFeature(GameAdditionalSkills)
   for i = Skill.Fist, Skill.ManaLeechAmount do
@@ -444,6 +484,337 @@ end
 
 function onBaseSpeedChange(localPlayer, baseSpeed)
   setSkillBase('speed', localPlayer:getSpeed(), baseSpeed)
+end
+
+function onManaLeechChange()
+  local skill = skillsWindow:recursiveGetChildById('manaleech')
+  if skill then
+    if manaLeechValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('manaleech', manaLeechValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onLifeLeechChange()
+  local skill = skillsWindow:recursiveGetChildById('lifeleech')
+  if skill then
+    if lifeLeechValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('lifeleech', lifeLeechValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onMagicLevelPointsChange()
+  local skill = skillsWindow:recursiveGetChildById('magicLevelPoints')
+  if skill then
+    if magicLevelPointsValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('magicLevelPoints', '+' .. magicLevelPointsValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onSkillSwordChange()
+  local skill = skillsWindow:recursiveGetChildById('skillSword')
+  if skill then
+    if skillSwordValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('skillSword', '+' .. skillSwordValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onSkillAxeChange()
+  local skill = skillsWindow:recursiveGetChildById('skillAxe')
+  if skill then
+    if skillAxeValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('skillAxe', '+' .. skillAxeValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onSkillClubChange()
+  local skill = skillsWindow:recursiveGetChildById('skillClub')
+  if skill then
+    if skillClubValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('skillClub', '+' .. skillClubValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onSkillDistanceChange()
+  local skill = skillsWindow:recursiveGetChildById('skillDistance')
+  if skill then
+    if skillDistanceValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('skillDistance', '+' .. skillDistanceValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onSkillShieldChange()
+  local skill = skillsWindow:recursiveGetChildById('skillShield')
+  if skill then
+    if skillShieldValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('skillShield', '+' .. skillShieldValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onExtraReflectChange()
+  local skill = skillsWindow:recursiveGetChildById('extraReflect')
+  if skill then
+    if extraReflectValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('extraReflect', extraReflectValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onExtraSpeedChange()
+  local skill = skillsWindow:recursiveGetChildById('extraSpeed')
+  if skill then
+    if extraSpeedValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('extraSpeed', '+' .. extraSpeedValue)
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onExtraManaPercentChange()
+  local skill = skillsWindow:recursiveGetChildById('extraManaPercent')
+  if skill then
+    if extraManaPercentValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('extraManaPercent', extraManaPercentValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onExtraHealthPercentChange()
+  local skill = skillsWindow:recursiveGetChildById('extraHealthPercent')
+  if skill then
+    if extraHealthPercentValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('extraHealthPercent', extraHealthPercentValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onAbsorbPercentPhysicalChange()
+  local skill = skillsWindow:recursiveGetChildById('absorbPercentPhysical')
+  if skill then
+    if absorbPercentPhysicalValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('absorbPercentPhysical', absorbPercentPhysicalValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onAbsorbPercentDeathChange()
+  local skill = skillsWindow:recursiveGetChildById('absorbPercentDeath')
+  if skill then
+    if absorbPercentDeathValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('absorbPercentDeath', absorbPercentDeathValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onAbsorbPercentFireChange()
+  local skill = skillsWindow:recursiveGetChildById('absorbPercentFire')
+  if skill then
+    if absorbPercentFireValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('absorbPercentFire', absorbPercentFireValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onAbsorbPercentEnergyChange()
+  local skill = skillsWindow:recursiveGetChildById('absorbPercentEnergy')
+  if skill then
+    if absorbPercentEnergyValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('absorbPercentEnergy', absorbPercentEnergyValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onAbsorbPercentEarthChange()
+  local skill = skillsWindow:recursiveGetChildById('absorbPercentEarth')
+  if skill then
+    if absorbPercentEarthValue > 0 then
+      skill:setVisible(true)
+      setSkillValue('absorbPercentEarth', absorbPercentEarthValue .. '%')
+    else
+      skill:setVisible(false)
+    end
+  end
+end
+
+function onPlayerStatsOpcode(protocol, opcode, buffer)
+  -- Faz parse do JSON
+  local json_status, json_data = pcall(function() return json.decode(buffer) end)
+  if not json_status then
+    return
+  end
+
+  -- Extrai o valor da chave manaLeech
+  if json_data and json_data.manaLeech then
+    local newValue = tonumber(json_data.manaLeech) or 0
+    manaLeechValue = newValue
+    onManaLeechChange()
+  end
+
+  -- Extrai o valor da chave lifeLeech
+  if json_data and json_data.lifeLeech then
+    local newValue = tonumber(json_data.lifeLeech) or 0
+    lifeLeechValue = newValue
+    onLifeLeechChange()
+  end
+
+  -- Extrai o valor da chave magicLevelPoints
+  if json_data and json_data.magicLevelPoints then
+    local newValue = tonumber(json_data.magicLevelPoints) or 0
+    magicLevelPointsValue = newValue
+    onMagicLevelPointsChange()
+  end
+
+  -- Extrai o valor da chave skillSword
+  if json_data and json_data.skillSword then
+    local newValue = tonumber(json_data.skillSword) or 0
+    skillSwordValue = newValue
+    onSkillSwordChange()
+  end
+
+  -- Extrai o valor da chave skillAxe
+  if json_data and json_data.skillAxe then
+    local newValue = tonumber(json_data.skillAxe) or 0
+    skillAxeValue = newValue
+    onSkillAxeChange()
+  end
+
+  -- Extrai o valor da chave skillClub
+  if json_data and json_data.skillClub then
+    local newValue = tonumber(json_data.skillClub) or 0
+    skillClubValue = newValue
+    onSkillClubChange()
+  end
+
+  -- Extrai o valor da chave skillDistance
+  if json_data and json_data.skillDistance then
+    local newValue = tonumber(json_data.skillDistance) or 0
+    skillDistanceValue = newValue
+    onSkillDistanceChange()
+  end
+
+  -- Extrai o valor da chave skillShield
+  if json_data and json_data.skillShield then
+    local newValue = tonumber(json_data.skillShield) or 0
+    skillShieldValue = newValue
+    onSkillShieldChange()
+  end
+
+  -- Extrai o valor da chave extraReflect
+  if json_data and json_data.extraReflect then
+    local newValue = tonumber(json_data.extraReflect) or 0
+    extraReflectValue = newValue
+    onExtraReflectChange()
+  end
+
+  -- Extrai o valor da chave extraSpeed
+  if json_data and json_data.extraSpeed then
+    local newValue = tonumber(json_data.extraSpeed) or 0
+    extraSpeedValue = newValue
+    onExtraSpeedChange()
+  end
+
+  -- Extrai o valor da chave extraManaPercent
+  if json_data and json_data.extraManaPercent then
+    local newValue = tonumber(json_data.extraManaPercent) or 0
+    extraManaPercentValue = newValue
+    onExtraManaPercentChange()
+  end
+
+  -- Extrai o valor da chave extraHealthPercent
+  if json_data and json_data.extraHealthPercent then
+    local newValue = tonumber(json_data.extraHealthPercent) or 0
+    extraHealthPercentValue = newValue
+    onExtraHealthPercentChange()
+  end
+
+  -- Extrai o valor da chave absorbPercentPhysical
+  if json_data and json_data.absorbPercentPhysical then
+    local newValue = tonumber(json_data.absorbPercentPhysical) or 0
+    absorbPercentPhysicalValue = newValue
+    onAbsorbPercentPhysicalChange()
+  end
+
+  -- Extrai o valor da chave absorbPercentDeath
+  if json_data and json_data.absorbPercentDeath then
+    local newValue = tonumber(json_data.absorbPercentDeath) or 0
+    absorbPercentDeathValue = newValue
+    onAbsorbPercentDeathChange()
+  end
+
+  -- Extrai o valor da chave absorbPercentFire
+  if json_data and json_data.absorbPercentFire then
+    local newValue = tonumber(json_data.absorbPercentFire) or 0
+    absorbPercentFireValue = newValue
+    onAbsorbPercentFireChange()
+  end
+
+  -- Extrai o valor da chave absorbPercentEnergy
+  if json_data and json_data.absorbPercentEnergy then
+    local newValue = tonumber(json_data.absorbPercentEnergy) or 0
+    absorbPercentEnergyValue = newValue
+    onAbsorbPercentEnergyChange()
+  end
+
+  -- Extrai o valor da chave absorbPercentEarth
+  if json_data and json_data.absorbPercentEarth then
+    local newValue = tonumber(json_data.absorbPercentEarth) or 0
+    absorbPercentEarthValue = newValue
+    onAbsorbPercentEarthChange()
+  end
 end
 
 function onMagicLevelChange(localPlayer, magiclevel, percent)
